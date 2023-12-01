0e7e0222392571d7a44bf89c40a6a23355471401
Maciek Grzelczak <mgrzelczak97@gmail.com>
2023-12-01 14:51:11 +0100

    Solved part two

--- src/Library/day1.fs
+++ src/Library/day1.fs
@@ -1,6 +1,29 @@
 namespace Library
 module day1 =
     let inline charToInt (c: char) = int c - int '0'
+    let replaceString (text: string) (newValue: string) (old: string) = 
+        text.Replace(old, newValue)
+    let wordToDigit (text: string) =
+        let replacements = [ ("one", "o1e");
+            ("two", "t2o");
+            ("three", "t3e");
+            ("four", "f4r");
+            ("five", "f5e");
+            ("six", "s6x");
+            ("seven", "s7n");
+            ("eight", "e8t");
+            ("nine", "n9e")
+            ]
+        let rec replaceAllOccurences text = function
+            | (word, value)::tail -> replaceAllOccurences (replaceString text value word) tail
+            | [] -> text
+
+        let rec matchText current = function
+            | head::tail -> matchText (replaceAllOccurences (current + string head) replacements) tail
+            | [] -> current 
+
+        // matchText "" (text |> List.ofSeq)
+        replaceAllOccurences text replacements
 
     let rec firstNumericChar = function
         | [] -> raise <| MatchFailureException("Cannot match", 1, 1)
@@ -22,7 +45,8 @@ module day1 =
     let solve: string =
         let sum = 0
         let lines = System.IO.File.ReadLines(fullPath)
-        let sum = Seq.map (fun x -> stringToCalibration x) lines |> Seq.sum
+        // Seq.iter (fun line -> printfn "%s" (wordToDigit line)) lines
+        let sum = Seq.map (fun line -> stringToCalibration (wordToDigit line)) lines |> Seq.sum
         printfn "Files read, solving"
 
         sprintf "%i" sum
