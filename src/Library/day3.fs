df8e96b3bc3fb9ffe9df9d9ee2cd22698fc0cb04
Maciek Grzelczak <mgrzelczak97@gmail.com>
2023-12-04 18:02:08 +0100

    Part 1 solved

--- src/Library/day3.fs
+++ src/Library/day3.fs
@@ -15,51 +15,55 @@ module day3 =
         | _ -> true
 
     let checkForSymbols (indexes: List<char>) = 
-        List.forall (fun c -> matchCharWithSymbol c) indexes
+        List.map (fun c -> matchCharWithSymbol c) indexes |> List.contains true
 
         
-
     let getIndexToCheck (line: string, index: int) =
         match index with
         | 0 -> [ line[0]; line[1]];
-        | x when x = (line.Length - 1) -> [line[x]; line[x - 1]]
+        | x when x = (line.Length - 1) -> printfn "final char"; [line[x]; line[x - 1]]
         | y -> [line[y - 1]; line[y]; line[y + 1]]
 
     let checkAllTheNeighbours (lines: List<string>, index: int) = function
         // Terminate early if already found
         | true -> true
-        | false -> List.forall (fun line -> checkForSymbols(getIndexToCheck(line, index))) lines
+        | false -> List.map (fun line -> checkForSymbols(getIndexToCheck(line, index))) lines |> List.contains true
 
-    let checkLineInput (line: string, prev: string, next: string, current: Option<string>, valid: bool, index: int) =
-        match line[index] with
-        | '.' -> current, valid
-        | x when charIsDigit x -> 
-            let newCurrent = 
-                match current with
-                | Some cur -> Some (cur + string x)
-                | None -> Some (string x)
-            newCurrent, checkAllTheNeighbours([prev; line; next], index) valid
-        | _ -> current, true 
+    let rec checkLineInput (line: string, prev: string, next: string, current: Option<string>, valid: bool, index: int, sum: int) =
+        if index = line.Length 
+            then match current with
+                    | None -> sum
+                    | Some x -> sum + (x |> int)
+            else
+                match line[index] with
+                | x when charIsDigit x -> 
+                    let newCurrent = 
+                        match current with
+                        | Some cur -> Some (cur + string x)
+                        | None -> Some (string x)
+                    checkLineInput(line, prev, next, newCurrent, valid || checkAllTheNeighbours([prev; line; next], index) valid, index + 1, sum)
+                | _ -> match valid with
+                            | false -> checkLineInput(line, prev, next, None, false, index + 1, sum)
+                            | true -> match current with
+                                        | Some x -> checkLineInput(line, prev, next, None, false, index + 1, sum + (x |> int))
+                                        | None -> checkLineInput(line, prev, next, None, false, index + 1, sum)
 
 
-    let parseLine (previous: string, current: string, next: string) =
-        // for i in 0..current.Length
+    let rec parseInput (input: List<string>, prev: Option<string>, current: Option<string>, sum: int) =
+        match input with
+            | [] -> sum
+            | head::tail -> match current with
+                            | None -> parseInput(tail, None, Some(head), sum)
+                            | Some currentVal -> match prev with
+                                                    | None -> parseInput(tail, current, Some(head), sum)
+                                                    | Some prevVal -> parseInput(tail, current, Some(head), sum + checkLineInput(currentVal, prevVal, head, None, false, 0, 0) )
 
-
-        21
-
-    let rec parseBlocks (remainer: List<string>, sum: int, previous: string, current: string) =
-        match remainer with
-        | [] -> sum
-        | head::tail -> parseBlocks (tail, sum + (parseLine (previous, current, head)), current, head)
 
     let solve: string =
         let fullPath = getFilePath "input3"
         let lines = System.IO.File.ReadLines(fullPath)
-        Seq.iter (fun line -> printfn "%A" (parseLine line)) lines
-        // Seq.map (fun line -> line) lines 
+        let paddingBlock = System.String.Concat([ for i in 1..(Seq.head lines).Length -> '.'])
+        let paddedLines = string paddingBlock :: (List.ofSeq lines) @ [string paddingBlock]
+        List.iter (fun x -> printfn "%s" x) paddedLines
+        sprintf "%i" (parseInput(paddedLines, None, None, 0))
 
-
-        printfn "Files read, solving"
-
-        "not solved yet"
